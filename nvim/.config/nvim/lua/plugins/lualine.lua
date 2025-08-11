581a1344607584e98367cbe028c1e352fb614c33
rflocfloc <riccardoscandino@gmail.com>
2025-08-11 17:37:01 +0200

    tmux dotfiles update

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -16,18 +16,19 @@ require("lualine").setup({
     lualine_a = { { "mode",  right_padding = 2 } },
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = { "filename" },
-    lualine_x = {"lsp_status", "filesize", "filetype"},
-    lualine_y = {
+    lualine_x = {"lsp_status"},
+    lualine_y = {"filesize", "filetype"},
+    lualine_z = {
       { "progress", separator = " ", padding = { left = 1, right = 0 } },
       { "location", padding = { left = 0, right = 1 } },
     },
-    lualine_z = {
-      {
-        function()
-          return " " .. os.date("%R")
-        end,
-      },
-    },
+    -- lualine_z = {
+    --   {
+    --     function()
+    --       return " " .. os.date("%R")
+    --     end,
+    --   },
+    -- },
   },
 })
 
