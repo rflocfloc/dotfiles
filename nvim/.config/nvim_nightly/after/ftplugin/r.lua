690d4dc86ae46a08bf6928b36899fa00e8aa3e1a
rflocfloc <riccardoscandino@gmail.com>
2025-09-11 14:59:02 +0200

    nvim nightly update

--- nvim/.config/nvim_nightly/after/ftplugin/r.lua
+++ nvim/.config/nvim_nightly/after/ftplugin/r.lua
@@ -2,4 +2,4 @@ vim.opt_local.tabstop = 4         -- Number of spaces a <Tab> counts for.
 vim.opt_local.shiftwidth = 4      -- Number of spaces to use for autoindent.
 vim.opt_local.spell = false       -- disable spell check for Python code (useful for functions, variables).
 
-vim.opt_local.makeprg = 'python3 %'
+vim.opt_local.makeprg = 'Rscript %'
