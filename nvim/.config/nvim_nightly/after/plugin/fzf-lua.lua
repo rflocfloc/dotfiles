0451c50db7e5f3c674f381960e7547859f44e922
rflocfloc <riccardoscandino@gmail.com>
2025-09-12 10:26:25 +0200

    ghostty update

--- nvim/.config/nvim_nightly/after/plugin/fzf-lua.lua
+++ nvim/.config/nvim_nightly/after/plugin/fzf-lua.lua
@@ -3,9 +3,6 @@ require 'fzf-lua'.setup({
     'telescope',
     actions = {
         files = {
-            -- instead of the default action 'actions.file_edit_or_qf'
-            -- it's important to define all other actions here as this
-            -- table does not get merged with the global defaults
             ["default"]       = actions.file_edit,
             ["ctrl-s"]        = actions.file_split,
             ["ctrl-v"]        = actions.file_vsplit,
@@ -20,4 +17,6 @@ local fzf = require('fzf-lua')
 vim.keymap.set('n', '<leader>sf', fzf.files, { desc = 'Search files' })
 vim.keymap.set('n', '<leader>sg', fzf.live_grep, { desc = 'Live grep' })
 vim.keymap.set('n', '<leader>sb', fzf.buffers, { desc = 'Search buffers' })
+vim.keymap.set('n', '<leader>sh', fzf.helptags, { desc = 'Search help' })
+vim.keymap.set('n', '<leader>sk', fzf.keymaps, { desc = 'Search keymaps' })
 
--- nvim/.config/nvim_nightly/init.lua
+++ nvim/.config/nvim_nightly/init.lua
@@ -8,10 +8,9 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter.git", version = 'main'},
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git", version = 'main' },
   { src = "https://github.com/ibhagwan/fzf-lua.git"},
+  -- Colorschemes
   { src = "https://github.com/rebelot/kanagawa.nvim.git"},
   { src = "https://github.com/rose-pine/neovim.git", name="rose-pine"},
-  { src = "https://github.com/shaunsingh/nord.nvim.git"},
-  { src = "https://github.com/folke/tokyonight.nvim.git"},
 })
 
 -- -----------------
@@ -22,8 +21,31 @@ vim.pack.add({
 vim.opt.background = 'dark'
 
 -- Load colorscheme with fallback
-local colorscheme = 'kanagawa-dragon'
+local colorscheme = 'TODO'
 local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 if not status_ok then
-  vim.cmd('colorscheme habamax')
+  vim.cmd('colorscheme default')
 end
+
+-- --------------------------
+-- [[ General Autocommands ]]
+-- --------------------------
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("User_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = augroup('HighlightText'),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    desc = 'Remove end of line whitespaces on writing',
+    group = augroup('RemoveWhitespaces'),
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
