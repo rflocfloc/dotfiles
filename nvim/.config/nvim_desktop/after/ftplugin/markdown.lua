e305907bc19069579d46d10d4f6c895e929939d5
rflocfloc <riccardoscandino@gmail.com>
2025-09-14 13:44:31 +0200

    moving nvim to separate repo

--- nvim/.config/nvim_desktop/after/ftplugin/markdown.lua
+++
@@ -1,9 +0,0 @@
-vim.opt_local.wrap = true      -- Enable line wrapping.
-vim.opt_local.linebreak = true -- Wrap long lines at a word boundary.
-vim.opt_local.textwidth = 160  -- Set text width, common for prose to improve readability.
-vim.opt_local.softtabstop = 2  -- Number of spaces a tab/backspace will insert/delete.
-vim.opt_local.shiftwidth = 2   -- Number of spaces to use for autoindent.
-
--- Enable spell check for Markdown as it's primarily text.
-vim.opt_local.spell = true                -- Turn on spell checking.
-vim.opt_local.spelllang = { 'en_us', 'it' } -- Set default spell language to US English.
--- nvim/.config/nvim_desktop/plugin/grepprg.lua
+++
@@ -1,5 +0,0 @@
-if vim.fn.executable('rg') == 1 then
-  vim.opt.grepprg = 'rg --vimgrep --hidden --no-heading '
-  vim.opt.grepformat = '%f:%l:%c:%m'
-end
-
